f061a41e843a4b080a2286348f2e884cf53f22de
robotty1234 <maoroboto@gmail.com>
2022-05-17 14:01:05 +0900

    secand commit

---
+++ launch/controlUD05.launch.py
@@ -0,0 +1,29 @@
+from os.path import join
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    pkg_prefix = get_package_share_directory('ud05_package')
+    ud05_node = Node(
+        package = 'ud05_package',
+        executable = 'ud05_runnning'
+    )
+    #return LaunchDescription([ud05_node])
+    
+    rightMotor = Node(
+        package = 'bipolar_stepper_package',
+        executable = 'bipolar_stepper',
+        name = 'RightMotor',
+        remappings=[('/stepperCmd', '/stepperCmdR'), ('/interrCmd', '/interrCmdR')],
+        parameters=[join(pkg_prefix, 'cfg/right_motor_parameter.yaml')]
+    )
+    leftMotor = Node(
+        package = 'bipolar_stepper_package',
+        executable = 'bipolar_stepper',
+        name = 'LeftMotor',
+        remappings=[('/stepperCmd', '/stepperCmdL'), ('/interrCmd', '/interrCmdL')],
+        parameters=[join(pkg_prefix, 'cfg/left_motor_parameter.yaml')]
+    )
+    return LaunchDescription([ud05_node, rightMotor, leftMotor])
+    
---
+++ src/ud05_runnning.cpp
@@ -0,0 +1,46 @@
+#include <rclcpp/rclcpp.hpp>
+#include <stepper_interfaces/msg/stepper_msgs.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <stdio.h>
+#include <unistd.h>
+using namespace std;
+using stepper_interfaces::msg::StepperMsgs;
+
+int main(int argc, char **argv){
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("UD_05");
+    auto pubCmdR = node->create_publisher<std_msgs::msg::String>("interrCmdR", 10);
+    auto pubStepperR = node->create_publisher<StepperMsgs>("stepperCmdR", 10);
+    auto pubCmdL = node->create_publisher<std_msgs::msg::String>("interrCmdL", 10);
+    auto pubStepperL = node->create_publisher<StepperMsgs>("stepperCmdL", 10);
+    RCLCPP_INFO(node->get_logger(), "Start ud05 package");
+    rclcpp::WallRate loop(1);
+    std_msgs::msg::String cmd;
+    StepperMsgs motorR;
+    StepperMsgs motorL;
+    motorR.step_specify = motorL.step_specify = false;
+    motorR.step_speed = motorL.step_speed = 50.0;
+    motorR.step_power = motorL.step_power = false;
+    motorR.step_val = 1;
+    motorL.step_val = -1;
+    sleep(5);
+    cmd.data = "STOP";
+    RCLCPP_INFO(node->get_logger(), "Publishing motor data");
+    RCLCPP_INFO(node->get_logger(), "motorR specify:%d, step_val:%d, step_speed:%f, step_power:%d", motorR.step_specify, motorR.step_val, motorR.step_speed, motorR.step_power);
+    RCLCPP_INFO(node->get_logger(), "motorL specify:%d, step_val:%d, step_speed:%f, step_power:%d", motorL.step_specify, motorL.step_val, motorL.step_speed, motorL.step_power);
+    pubStepperR->publish(motorR);
+    pubStepperL->publish(motorL);
+    loop.sleep();
+    sleep(3);
+    pubCmdR->publish(cmd);
+    pubCmdL->publish(cmd);
+    loop.sleep();
+    RCLCPP_INFO(node->get_logger(), "Finish ud05 package");
+    rclcpp::shutdown();
+    return 0;
+}
+
+
